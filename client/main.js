8f9f340ba200f92eb43c1267c78ae02c117dc107
imironjin <cjfwls9806@gmail.com>
2023-01-19 16:09:59 +0900

    storage 학습

--- client/main.js
+++ client/main.js
@@ -1,1 +1,16 @@
-console.log(window);
+
+import { getNode, loadStorage, saveStorage } from "./lib/index.js";
+
+
+const textField = getNode('#tetField');
+const deleteButton = getNode('input[value="삭제"]');
+
+
+loadStorage('area').then((res)=>{
+  textField.value = res;
+})
+function inputHandler() {
+  saveStorage('area',textField.value);
+}
+
+textField.addEventListener('input',inputHandler);
